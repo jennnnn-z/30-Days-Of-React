f5f25a8a7b92ad9b308af844268d8d673bb63c49
Jennifer Zhang <71909797+jennnnn-z@users.noreply.github.com>
2023-08-02 19:54:52 +0000

    went thru days 1-2

---
+++ solutions/day-03/day3/src/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import screenshot from './images/Screenshot 2023-06-06 102101.png'
+
+
+// JSX element, header
+const welcome = 'Welcome to 30 Days Of React Challenge'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 2, 2020'
+
+// JSX element, header
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+const yearBorn = 1820
+const currentYear = 2020
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+// User Card Component
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={screenshot} alt='screenshot' />
+    <h2>
+      {author.firstName} {author.lastName}
+    </h2>
+  </div>
+)
+
+// JSX element, main
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+
+// JSX element, main
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <div>
+        <p>
+          Prerequisite to get started{' '}
+          <strong>
+            <em>react.js</em>
+          </strong>
+          :
+        </p>
+        <ul>{techsFormatted}</ul>
+        {result}
+        {personAge}
+      </div>
+      <UserCard />
+    </div>
+  </main>
+)
+
+const copyRight = '2020'
+
+// JSX element, footer
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright &copy;{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// JSX element, app
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+// To get the root element from the HTML document
+const rootElement = document.querySelector('.root')
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
